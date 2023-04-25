9f1e1ee004f33c814b4c9b52f026331e3007e13a
lpdervaux <lp.dervaux@proton.me>
2023-04-25 20:37:52 +0200

    [Change] LocationFixtures refactor

--- src/DataFixtures/LocationFixtures.php
+++ src/DataFixtures/LocationFixtures.php
@@ -5,74 +5,46 @@ declare(strict_types=1);
 namespace App\DataFixtures;
 
 use App\Entity\Location;
-use App\Service\FakerService;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class LocationFixtures extends Fixture implements DependentFixtureInterface
+class LocationFixtures
+    extends FakerFixtures
+    implements DependentFixtureInterface
 {
-    public const LOCATION_COUNT = 40;
-    public const LOCATION_PREFIX = 'location_';
-
-    private readonly ObjectManager $manager;
-    private readonly array $cities;
-
-    public function __construct (
-        private readonly FakerService $fakerService
-    ) {}
+    public const COUNT = 40;
 
     public function getDependencies () : array
     {
-        return [CityFixtures::class];
+        return [ CityFixtures::class ];
     }
 
     public function load (ObjectManager $manager) : void
     {
-        $this->initialize($manager);
+        parent::load($manager);
 
-        $this->fakeMany(self::LOCATION_COUNT);
+        $this->fakeMany(self::COUNT);
 
         $manager->flush();
     }
 
-    private function initialize (ObjectManager $manager) : void
-    {
-        $this->manager = $manager;
-
-        $cities = [];
-        for ($i = 0; $i < CityFixtures::COUNT; $i++) {
-            $cities[] = $this->getReference(CityFixtures::class . $i);
-        }
-        $this->cities = $cities;
-    }
-
-    private function fakeMany (int $count) : void
-    {
-        for ($i = 0; $i < $count; $i++) {
-            $location = $this->generateFake();
-            $this->manager->persist($location);
-            $this->addReference(self::LOCATION_PREFIX . $i, $location);
-        }
-    }
-
-    private function generateFake () : Location
+    protected function generate () : Location
     {
         return (new Location())
             ->setName(
-                $this->fakerService->getGenerator()->unique()->company()
+                $this->generator->unique()->company()
             )
             ->setAddress(
-                $this->fakerService->getGenerator()->address()
+                $this->generator->address()
             )
             ->setCity(
-                $this->fakerService->getGenerator()->randomElement($this->cities)
+                $this->getReference(CityFixtures::class . mt_rand(0, CityFixtures::COUNT - 1))
             )
             ->setLatitude(
-                $this->fakerService->getGenerator()->latitude()
+                $this->generator->latitude()
             )
             ->setLongitude(
-                $this->fakerService->getGenerator()->longitude()
+                $this->generator->longitude()
             );
     }
 }
