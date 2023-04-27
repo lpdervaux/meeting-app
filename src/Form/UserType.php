4bc2af69dddd6f5f727f9daf8609752af83e84ea
Rayana Dachaeva <rayana.d333@gmail.com>
2023-04-27 16:24:49 +0200

    Mise à jour

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,24 +3,23 @@
 namespace App\Form;
 
 use App\Entity\Campus;
-use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
-class ProfilFormType extends AbstractType
+class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('nickname', TextType::class, [
-                'label' => 'Pseudo'
+                'label' => 'Pseudo',
             ])
             ->add('surname', TextType::class, [
                 'label' => 'Prénom'
@@ -43,7 +42,8 @@ class ProfilFormType extends AbstractType
                     'autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'groups' => ['new'],
+                        'message' => 'Password should not be blank.',
                     ]),
                     new Length([
                         'min' => 6,
@@ -52,22 +52,19 @@ class ProfilFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-                'first_options' => ['label' => 'Mot de passe'],
-                'second_options' => ['label' => 'Confirmation']
+                'first_options' => ['label' => 'Mot de passe', 'required' => false],
+                'second_options' => ['label' => 'Confirmation','required' => false]
             ])
             ->add('campus', EntityType::class, [
                 'label' => 'Campus',
                 'class' => Campus::class,
                 'choice_label' => 'name',
             ])
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Télécharger vers le server',
+                'required' => false,
+            ])
 
         ;
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-        ]);
     }
 }
