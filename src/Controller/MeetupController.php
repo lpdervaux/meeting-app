abe76995094fb2d11b60115df67747977985a3e7
Donatien POULET <donatien.poulet2022@campus-eni.fr>
2023-04-27 22:14:40 +0200

    modif MeetupController

--- src/Controller/MeetupController.php
+++ src/Controller/MeetupController.php
@@ -3,16 +3,59 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MeetupController extends AbstractController
 {
     #[Route('/meetup', name: 'app_meetup_list')]
-    public function list(): Response
+    public function list(Request $request): Response
     {
+        $form = $this->createFormBuilder()
+            ->add('campus', EntityType::class, [
+                'class' => Campus::class,
+                'label' => 'Campus : ',
+                'choice_label' => 'name'
+            ])
+            ->add('research', TextType::class , [
+                'label' => 'Le nom de la sortie contient : ',
+                'required' => false
+            ])
+            ->add('start', DateType::class, [
+                'label' => 'Entre : ',
+                'html5'=> true,
+                'widget' => 'single_text',
+                'input' => 'datetime_immutable',
+                'required' => false
+            ])
+            ->add('end', DateType::class, [
+                'label' => 'et : ',
+                'html5'=> true,
+                'widget' => 'single_text',
+                'input' => 'datetime_immutable',
+                'required' => false
+            ])
+            ->add('coordinator', CheckboxType::class, [
+                'label' => 'Sorties dont je suis l\'organisateur/trice : ',
+                'required' => false
+            ])
+            ->add('registered', CheckboxType::class, [
+                'label' => 'Sorties auxquelles je suis inscrit/e : ',
+                'required' => false
+            ])
+            ->add('no_registered', CheckboxType::class, [
+                'label' => 'Sorties auxquelles je ne suis pas inscrit/e : ',
+                'required' => false
+            ])
+            ->add('past', CheckboxType::class, [
+                'label' => 'Sorties passées : ',
+                'required' => false
+            ])
+            ->getForm();
+
         return $this->render('meetup/index.html.twig', [
-            'controller_name' => 'MeetupController',
+            'form' => $form,
         ]);
     }
 }
