40601c09133f478dcfb2683e4e92ddab19ee3a43
lpdervaux <lp.dervaux@proton.me>
2023-04-30 16:11:19 +0200

    [Add] MeetupController new and details

--- src/Controller/MeetupController.php
+++ src/Controller/MeetupController.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Meetup;
+use App\Entity\MeetupStatus;
 use App\Entity\User;
+use App\Form\MeetupCancelType;
+use App\Form\MeetupRegisterUserType;
 use App\Form\MeetupType;
 use App\Repository\MeetupRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -18,15 +21,92 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/meetup', name: 'app_meetup')]
 class MeetupController extends AbstractController
 {
+    #[Route(
+        '/{id}/details',
+        name: '_details',
+        requirements: [ 'id' => '^\d+$' ]
+    )]
+    public function details (
+        int $id,
+        Request $request,
+        MeetupRepository $meetupRepository,
+        EntityManagerInterface $entityManager
+    ) : Response
+    {
+        $meetup = $meetupRepository->findDetails($id);
+        if ( ! $meetup )
+            throw $this->createNotFoundException();
+        $user = $this->getUser();
+        if ( ! $user instanceof User )
+            throw $this->createAccessDeniedException();
+
+        $registrationEnabled = $meetup->canRegister($user);
+        $cancelEnabled = ( ! $meetup->isCancelled() )
+            && ( $user === $meetup->getCoordinator() );
+        $now = new \DateTimeImmutable();
+        $cancelAlert = ( $meetup->isCancelled() )
+            && ( $now < $meetup->getEnd() );
+
+        $registerForm = $this->createForm(MeetupRegisterUserType::class);
+        $cancelForm = $this->createForm(MeetupCancelType::class);
+
+        $registerForm->handleRequest($request);
+        if ( $registerForm->isSubmitted() && $registerForm->isValid() )
+        {
+            if ( $registrationEnabled )
+            {
+                $meetup->addAttendee($user);
+                $entityManager->persist($meetup);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Inscription réussie');
+
+                $registrationEnabled = false;
+            }
+            else
+                $this->addFlash('warning', 'Inscription échouée');
+        }
+
+        $cancelForm->handleRequest($request);
+        if ( $cancelForm->isSubmitted() && $cancelForm->isValid() )
+        {
+            if ( $cancelEnabled )
+            {
+                $meetup->setCancelled(true);
+                $meetup->setCancellationDate(new \DateTimeImmutable());
+                $meetup->setCancellationReason($cancelForm->getData()['cancellationReason']);
+                $entityManager->persist($meetup);
+                $entityManager->flush($meetup);
+
+                $cancelEnabled = false;
+            }
+        }
+
+        return $this->render(
+            'meetup/details.html.twig',
+            [
+                'meetup' => $meetup,
+
+                'registerFormView' => $registerForm->createView(),
+                'cancelFormView' => $cancelForm->createView(),
+
+                'registrationEnabled' => $registrationEnabled,
+                'cancelEnabled' => $cancelEnabled,
+                'cancelAlert' => $cancelAlert
+            ]
+        );
+    }
+
     #[Route('/new', name: '_new')]
     public function new (Request $request, EntityManagerInterface $entityManager) : Response
     {
         $user = $this->getUser();
         if ( ! $user instanceof User )
-            throw new HttpException(statusCode: 500);
+            throw $this->createAccessDeniedException();
 
         $meetup = new Meetup();
         $meetup->setCoordinator($user);
+        $meetup->addAttendee($user);
         $meetup->setCancelled(false);
 
         $form = $this->createForm(MeetupType::class, $meetup);
@@ -49,18 +129,5 @@ class MeetupController extends AbstractController
             );
 
         return $response;
-    }
-
-    #[Route('/details/{id}', name: '_details')]
-    public function details (int $id, MeetupRepository $meetupRepository) : Response
-    {
-        $meetup = $meetupRepository->find($id);
-        if ( $meetup )
-            $this->createNotFoundException();
-
-        return $this->render(
-            'meetup/details.html.twig',
-            [ 'meetup' => $meetup ]
-        );
     }
 }
---
+++ src/Form/MeetupRegisterUserType.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+
+class MeetupRegisterUserType extends AbstractType {}
