5c44cf56ec9c3a854e7dbefe9b99a500c5ce72d2
lpdervaux <lp.dervaux@proton.me>
2023-05-02 23:54:29 +0200

    [Fix] Handle null validation on CompoundLocationConstraintValidator

--- src/Validator/CompoundLocationConstraintValidator.php
+++ src/Validator/CompoundLocationConstraintValidator.php
@@ -11,12 +11,20 @@ class CompoundLocationConstraintValidator extends ConstraintValidator
 {
     public function validate (mixed $value, Constraint $constraint) : void
     {
-        $violations = $this->context->getValidator()->validate($value);
-
-        if ( $violations->count() > 0 )
+        if ( ! $value )
             $this->context
-                ->buildViolation('Invalid location')
+                ->buildViolation('Please select a valid location')
                 ->atPath('location')
                 ->addViolation();
+        else
+        {
+            $violations = $this->context->getValidator()->validate($value);
+
+            if ( $violations->count() > 0 )
+                $this->context
+                    ->buildViolation('Please fill new location information.')
+                    ->atPath('location') // TODO: find how to map the error to the correct fields
+                    ->addViolation();
+        }
     }
 }
